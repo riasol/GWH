195c2d2268681926784cfc6ccbb104da3fdc27d6
riasol <riasol.rob@gmail.com>
2012-08-11 04:24:46 +0200

    Basic skeletons

--- core/src/main/java/com/handfree/core/GWH.java
+++ core/src/main/java/com/handfree/core/GWH.java
@@ -1,27 +1,48 @@
 package com.handfree.core;
 
-import static playn.core.PlayN.*;
+import static playn.core.PlayN.assets;
+import static playn.core.PlayN.graphics;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import playn.core.Game;
 import playn.core.Image;
 import playn.core.ImageLayer;
 
 public class GWH implements Game {
-  @Override
-  public void init() {
-  }
+    private Play activePlay;
+    private final List<Play> plays = new ArrayList<Play>();
+    {
+	plays.add(new PearsGame());
+	plays.add(new MazeGame());
+    }
 
-  @Override
-  public void paint(float alpha) {
-    // the background automatically paints itself, so no need to do anything here!
-  }
+    @Override
+    public void init() {
+	Image bgImage = assets().getImage("images/bg.png");
+	ImageLayer bgLayer = graphics().createImageLayer(bgImage);
+	graphics().rootLayer().add(bgLayer);
+	startGame(1);
+    }
 
-  @Override
-  public void update(float delta) {
-  }
+    private void startGame(int index) {
+	activePlay = plays.get(index);
+	activePlay.init();
+    }
 
-  @Override
-  public int updateRate() {
-    return 25;
-  }
+    @Override
+    public void paint(float alpha) {
+	activePlay.paint(alpha);
+    }
+
+    @Override
+    public void update(float delta) {
+	activePlay.update(delta);
+    }
+
+    @Override
+    public int updateRate() {
+	return 25;
+    }
 }
